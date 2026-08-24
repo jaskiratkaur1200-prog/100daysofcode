8355222108fa83d5d0135ec632e0c33c6cb96f27
Jaskirat Kaur <jaskirat@jaskirats-MacBook-Air.local>
2026-08-24 13:33:56 +0530

    day15q29

---
+++ day15q29.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int n,factorial=1;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        factorial= factorial * i;
+}
+printf("Factorial of %d is: %d",n,factorial);
+}
